d56e052fe456900f406abcb7da6e729fdddbe4d5
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-02-09 08:36:15 +0530

    practise of usesate

--- hello/src/App.jsx
+++ hello/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from './components/Header'
 import Foot from './components/Footer'
 import Main from './components/Main'
 import Curly from './components/cutlrybarces'
 
 const App = () => {
+
+  const [count,setcount]=useState(0)
+  
   return (
       <>
+      <h1>{count}</h1>
+      <button onClick={()=>{
+        setcount(count+1)
+      }}></button>
     <Head />
     <Main />
     <Foot />
