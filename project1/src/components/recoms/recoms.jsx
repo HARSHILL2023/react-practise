20582215d9b83092348c59f5bc4a8850cd92e079
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-02-12 21:08:20 +0530

    finishing todays work only sidebar is remainng

--- project1/src/components/recoms/recoms.jsx
+++ project1/src/components/recoms/recoms.jsx
@@ -3,9 +3,16 @@ import './recoms.css'
 
 const recoms = () => {
   return (
-    <div>
-      <h1>remcomdede</h1>
+   <>
+    <h2>recomended </h2>
+    <div className="recoms-button">
+      <button className='btns'>all products</button>
+      <button className='btns'>nike</button>
+      <button className='btns'>puma</button>
+      <button  className='btns'>vans</button>
     </div>
+   
+   </>
   )
 }
 
