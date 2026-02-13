61d0c7de6ebb3e4770ae592022f3440adf9056e2
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-02-13 21:12:27 +0530

    changes are nmade:

--- react-typescipt/intro/src/components/usr.tsx
+++ react-typescipt/intro/src/components/usr.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+// import React from 'react'
 
-const Usr = ({name,age,ismaried}: {name: string; age:number,ismaried:Boolean;}) => {
+const Usr = ({name,age,}: {name: string; age:number,ismaried:Boolean;}) => {
   return (
     <>  
     <h1>{name}</h1>
