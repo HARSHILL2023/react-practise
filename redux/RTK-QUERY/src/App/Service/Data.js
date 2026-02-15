08b67dcff0444c36c5f0aefdc1d6b437e3517915
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-02-15 12:21:10 +0530

    practise query

--- redux/RTK-QUERY/src/App/Service/Data.js
+++
@@ -1,9 +0,0 @@
-import {createApi,fetchbaseQuery} from '@reduxjs/toolkit/query/react'
-
-createApi({
-    reducerPath : "products",
-    baseQuery:fetchbaseQuery({baseUrl:"https://dummyjson.com"}),
-    endpoints : (builder) => ({
-        get 
-    })
-})
--- redux/RTK-QUERY/src/main.jsx
+++ redux/RTK-QUERY/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { store } from './App/Store.js'
-import {provider} from 'react-redux'
+import {provider} from "react-redux"
 
 createRoot(document.getElementById('root')).render(
   <provider store={store}>
