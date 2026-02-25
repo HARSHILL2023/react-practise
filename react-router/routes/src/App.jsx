39b4ddd4586e80b9977f99d9bff6b3c11a911ef1
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-02-25 10:51:33 +0530

    changed

---
+++ react-router/routes/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route ,Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+        <Routes>
+        <Route  path='/about'   element={<about />}  />
+        </Routes>
+    </>
+  )
+}
+
+export default App
